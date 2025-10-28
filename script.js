519b0b378d2613a44a0411945b29a1a87762c83a
Aefrly <fraley.allison@ic.edu>
2025-10-28 10:45:41 -0500

    Created the getDogFact function using the .then() method. I had to comment out the other function using the async/await to get this one to work.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let data = fetch('https://dogapi.dog/api/facts');
 console.log(data);
 
 //Sending API requests using async/await
-async function getDogFact() {
+/*async function getDogFact() {
   const response = await fetch("https://dogapi.dog/api/facts");
   const data = await response.json();
   const fact = data.facts[0];
@@ -17,4 +17,27 @@ async function getDogFact() {
 const button = document.getElementById('fetch-dog-btn');
 button.addEventListener('click', function() {
     getDogFact();
+});*/
+
+//Sending API requests using .then()
+function getDogFact() {
+  fetch("https://dogapi.dog/api/facts")
+    .then(response => response.json())  // Covert the response
+    .then(data => { 
+      // Update the DOM with the dog fact
+      const fact = data.facts[0]; 
+      const output = document.getElementById("fact-output"); 
+      output.textContent = fact; 
+    }) 
+    .catch(error => { 
+      // Message to print if there was an error
+      console.error("Error fetching dog fact:", error); 
+    });
+}
+
+// Same button code as async/await 
+const button = document.getElementById("fetch-dog-btn"); 
+
+button.addEventListener("click", function () { 
+  getDogFact(); 
 });
