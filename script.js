d07a436f5b99bf37efea30af30c9e9f974a6a859
Aefrly <fraley.allison@ic.edu>
2025-10-28 10:39:42 -0500

    Created the getDogFact function using an asynchronous operation and await operator.

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ console.log("script.js loaded");
 
 let data = fetch('https://dogapi.dog/api/facts');
 console.log(data);
+
+async function getDogFact() {
+  const response = await fetch("https://dogapi.dog/api/facts");
+  const data = await response.json();
+  const fact = data.facts[0];
+  
+  // Update the DOM with the dog fact
+  const output = document.getElementById("dog-fact-output"); 
+  output.textContent = fact;
+}
+
+getDogFact();
